875fda377fd80ea2731556b43b4e54cb6223f46f
An-jonghun <dkswhdgnsvv@gmail.com>
2020-01-10 16:15:29 +0900

    secondCommit.2010-01-10

--- app/src/main/java/com/example/vedioplaytest/MainActivity.java
+++ app/src/main/java/com/example/vedioplaytest/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.vedioplaytest;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.SurfaceTexture;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
@@ -12,13 +17,11 @@ import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,TextureView.SurfaceTextureListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int SELECT_GALLERY = 1;
     private static final int SELECT_INTERNET = 2;
     public static int GET_VIEOTYPE = 0;
-    public final static int URL = 1;
-    public final static int SDCARD = 2;
 
     private Button btnStart;
     private Button btnPause;
@@ -26,13 +29,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnGetVedioGallery;
     private Button btnGetVedioInternet;
 
-    private VideoView videoView;    //비교할 영상
-    private TextureView myActionView;       // 내동작
+     VideoView videoView;    //비교할 영상
+     TextureView myActionView;       // 내동작
 
-    Intent intentGetVideo;
+    MediaController mediaController;
 
+    String videoPath;
     boolean videoReady = false; //  비디오영상이 준비됬는지 안됬는지 확인
 
+    Intent intentGetVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +47,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         videoView = findViewById(R.id.videoView);
         myActionView = findViewById(R.id.myAction);
-
-        myActionView.setSurfaceTextureListener(this);
 
         btnStart = findViewById(R.id.btnStart);
         btnPause = findViewById(R.id.btnPause);
@@ -55,14 +58,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnStart.setOnClickListener(this);
         btnPause.setOnClickListener(this);
         btnRestart.setOnClickListener(this);
+        btnGetVedioGallery.setOnClickListener(this);
+        btnGetVedioInternet.setOnClickListener(this);
 
+        mediaController = new MediaController(this);
 
-        MediaController mediaController = new MediaController(this);
-        VideoSetPath videoSetPath = new VideoSetPath(videoView, mediaController);
+       /* VideoSetPath videoSetPath = new VideoSetPath(videoView, mediaController);
         videoReady = videoSetPath.isVideoReady();
+        //테스트값*/
 
-/*        if (GET_VIEOTYPE != 0) {    //갤러리나 인터넷중에 선택했을경우
-            VideoSetPath videoSetPath = new VideoSetPath(videoView, mediaController, GET_VIEOTYPE);
+        if (GET_VIEOTYPE != 0) {    //갤러리나 인터넷중에 선택했을경우
+            VideoSetPath videoSetPath = new VideoSetPath(videoView, mediaController, GET_VIEOTYPE,videoPath);
+
+            Log.e("경로값",videoPath);
 
             if (GET_VIEOTYPE == GET_VIEOTYPE) {     //갤러리에서 선택한 비디오영상 재생
                 videoReady = videoSetPath.isVideoReady();
@@ -72,7 +80,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } else {        //아무것도 선택안했을경우
                 Toast.makeText(this, "갤러리나 인터넷에서 영상을 선택해주세요.", Toast.LENGTH_SHORT).show();
             }
-        }*/
+        }
     }
 
     @Override
@@ -89,49 +97,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     videoView.seekTo(0);
                     videoView.start();
                     break;
-
-                /*case R.id.getVedioGallery:
-                    GET_VIEOTYPE = SELECT_GALLERY;
-                    intentGetVideo = new Intent(Intent.ACTION_GET_CONTENT);
-                    intentGetVideo.setType("video/*");
-                    startActivityForResult(intentGetVideo, SELECT_GALLERY);
-                    break;
-                case R.id.getVedioInternet:
-                    GET_VIEOTYPE = SELECT_INTERNET;
-                    intentGetVideo = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com"));
-                    //TODO 인터넷에서 동영상 String path값 받기;
-                    break;*/
             }
         } else {
+           if(v.getId()==R.id.btnStart || v.getId()==R.id.btnPause || v.getId()==R.id.btnRestart)
             Toast.makeText(this, "재생에 문제가 있습니다.", Toast.LENGTH_SHORT).show();
+        }
+
+        switch (v.getId()) {
+            case R.id.getVedioGallery:
+                GET_VIEOTYPE = SELECT_GALLERY;
+                intentGetVideo = new Intent(Intent.ACTION_GET_CONTENT);
+                intentGetVideo.setType("video/*");
+                startActivityForResult(intentGetVideo, SELECT_GALLERY);
+                break;
+            case R.id.getVedioInternet:
+                GET_VIEOTYPE = SELECT_INTERNET;
+                intentGetVideo = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com"));
+                startActivity(intentGetVideo);
+                //  @TODO: 인터넷에서 동영상 String path값 받기;
+                break;
         }
     }
 
     @Override
-
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        videoPath = getRealPathFromURI(uri);
     }
 
-    @Override
-    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+    private String getRealPathFromURI(Uri uri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor c = getContentResolver().query(uri, proj, null, null, null);
+        int index = c.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
 
-    }
+        c.moveToFirst();
+        String path = c.getString(index);
 
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
-    }
-
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-        return false;
-    }
-
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-
+        return path;
     }
 }
 
--- app/src/main/java/com/example/vedioplaytest/VideoSetPath.java
+++ app/src/main/java/com/example/vedioplaytest/VideoSetPath.java
@@ -1,13 +1,14 @@
 package com.example.vedioplaytest;
 
+import android.net.Uri;
 import android.os.Environment;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideoSetPath {
 
-    public final static int URL = 1;
-    public final static int SDCARD = 2;
+    public final static int SELECT_GALLERY = 1;
+    public final static int SELECT_INTERNET = 2;
 
     boolean videoReady = false;
 
@@ -16,26 +17,28 @@ public class VideoSetPath {
 
     public VideoSetPath(VideoView videoView, MediaController mediaController) {
 
-                VIDEO_Path = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4"; //추후변경
-                videoView.setVideoPath(VIDEO_Path);
+        VIDEO_Path = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4"; //추후변경
+
+        출처: https://tony-programming.tistory.com/entry/Android-VideoView-예제 [Tony Programming]
+
+        videoView.setVideoURI(Uri.parse(VIDEO_Path));
 
         videoView.setMediaController(mediaController);
         videoReady = true;  //동영상재생 준비완료
     }
 
-    public VideoSetPath(VideoView videoView, MediaController mediaController, int type) {
+    public VideoSetPath(VideoView videoView, MediaController mediaController, int GET_VIEOTYPE, String VIDEO_Path) {
 
-        switch (type) {
-            case URL:
-                //동영상 경로가 URL일 경우
-                VIDEO_Path = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4"; //추후변경
-                videoView.setVideoPath(VIDEO_Path);
+        switch (GET_VIEOTYPE) {
+            case SELECT_GALLERY:
+                //동영상 경로가 내갤러리일 경우
+                this.VIDEO_Path = VIDEO_Path;
+                videoView.setVideoPath(this.VIDEO_Path);
                 break;
-            case SDCARD:
-                //동영상 경로가 SDCARD일 경우
-                //VIDEO_Path = Environment.getExternalStorageDirectory() + "/TestVideo.mp4";
+            case SELECT_INTERNET:
+                //동영상 경로가 인터넷일 경우
                 VIDEO_Path = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4"; //추후변경
-                videoView.setVideoPath(VIDEO_Path);
+            videoView.setVideoURI(Uri.parse(VIDEO_Path));
                 break;
         }
         videoView.setMediaController(mediaController);
